1fbb025f72b06a462b106eebe341e24ea5e67299
Roman Ortynskyi <50491616+lazyDefender@users.noreply.github.com>
2023-11-10 21:23:52 +0200

    Added docker

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,12 @@ import { AppModule } from './app.module'
 import { graphqlUploadExpress } from 'graphql-upload'
 
 async function bootstrap() {
+  const port = process.env.PORT || 3000
   const app = await NestFactory.create(AppModule)
 
   app.use(graphqlUploadExpress({ maxFileSize: 10000000, maxFiles: 10 }))
 
-  await app.listen(3000)
+  await app.listen(port)
 }
 
 bootstrap()
