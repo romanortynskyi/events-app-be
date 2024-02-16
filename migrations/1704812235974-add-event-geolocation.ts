b62ce37d3e107b2d886e6e6ab6c74ec125b23201
Roman Ortynskyi <50491616+lazyDefender@users.noreply.github.com>
2024-02-16 16:37:24 +0200

    Progress

--- migrations/1704812235974-add-event-geolocation.ts
+++ migrations/1704812235974-add-event-geolocation.ts
@@ -3,18 +3,18 @@ import { MigrationInterface, QueryRunner } from 'typeorm'
 class AddEventGeolocation1704812235974 implements MigrationInterface {
   name = 'AddEventGeolocation1704812235974'
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-      await queryRunner.query(`
-				ALTER TABLE "event"
-				ADD "geolocation"
-				geography(Point,4326)
-			`)
-      await queryRunner.query(`
-				CREATE INDEX "IDX_a321758459b60a2b8340ee131e"
-				ON "event"
-				USING GiST ("geolocation")
-			`)
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+			ALTER TABLE "event"
+			ADD "geolocation"
+			geography(Point,4326)
+		`)
+    await queryRunner.query(`
+			CREATE INDEX "IDX_a321758459b60a2b8340ee131e"
+			ON "event"
+			USING GiST ("geolocation")
+		`)
+  }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`DROP INDEX "public"."IDX_a321758459b60a2b8340ee131e"`)
